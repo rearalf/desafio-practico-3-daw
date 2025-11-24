1b4aae21d265d330f7f1849384c602d2339e4b41
Ricardo Alf <rear_recinos@hotmail.com>
2025-11-24 15:56:45 -0600

    feat: Add sweet alert

--- src/app/cart/cart-modal.ts
+++ src/app/cart/cart-modal.ts
@@ -1,13 +1,14 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../services/cart.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart-modal',
   templateUrl: './cart-modal.html',
   styleUrls: ['./cart-modal.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
 export class CartModalComponent {
   @Output() close = new EventEmitter<void>();
@@ -25,7 +26,18 @@ export class CartModalComponent {
     this.total = this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
   }
 
-  cerrar() {
+  closed() {
     this.close.emit();
+  }
+
+  pay() {
+    Swal.fire({
+      title: 'Transacción exitosa!',
+      text: 'Su pago se realizo con exito',
+      icon: 'success',
+      timer: 2000,
+      showConfirmButton: false,
+    });
+    this.closed();
   }
 }
