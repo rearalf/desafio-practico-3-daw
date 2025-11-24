093752488a2081148c313a7d8453992bc767d535
Ricardo Alf <rear_recinos@hotmail.com>
2025-11-24 08:38:29 -0600

    feat: Add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { HomeComponent } from './home/home';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
+ 
